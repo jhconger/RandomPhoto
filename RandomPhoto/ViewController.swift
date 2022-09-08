550fbd4238e6f808ee022420b68c34c6fe1c2864
jhconger <jhconger@gmail.com>
2022-09-07 21:05:42 -0500

    Initial Commit

---
+++ RandomPhoto/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RandomPhoto
+//
+//  Created by justin conger on 9/7/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
