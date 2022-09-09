febe7d96ac412a6b778f87ee450052e6284fcd48
jhconger <jhconger@gmail.com>
2022-09-09 16:29:50 -0500

    added button to load new photo

--- RandomPhoto/ViewController.swift
+++ RandomPhoto/ViewController.swift
@@ -10,6 +10,13 @@ class ViewController: UIViewController {
         imageView.backgroundColor = .white
         return imageView
     }()
+    private let button: UIButton  = {
+        let button = UIButton()
+        button.backgroundColor = .white
+        button.setTitle("Random Photo", for: .normal)
+        button.setTitleColor(.black, for: .normal)
+        return button
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -17,28 +24,18 @@ class ViewController: UIViewController {
         view.addSubview(imageView)
         imageView.frame = CGRect(x: 0, y: 0, width: 300, height: 300)
         imageView.center = view.center
+        view.addSubview(button)
         getRandomPhoto()
-        let button = UIButton(frame: CGRect(x: 100,
-                                                  y: 800,
-                                                  width: 200,
-                                                  height: 60))
-              button.setTitle("Save",
-                              for: .normal)
-              button.setTitleColor(.systemBlue,
-                                   for: .normal)
-              
-              button.addTarget(self,
-                               action: #selector(buttonAction),
-                               for: .touchUpInside)
-              
-              self.view.addSubview(button)
-              
-              // Do any additional setup after loading the view.
-          }
+        button.addTarget(self, action: #selector(didTapButton), for: .touchUpInside)
+    }
+    @objc func didTapButton() {
+        getRandomPhoto()
 
-          @objc
-          func buttonAction() {
-              print("Button pressed")
+    }
+        override func viewDidLayoutSubviews() {
+            super.viewDidLayoutSubviews()
+            button.frame = CGRect(x: 30, y: view.frame.size.height-150-view.safeAreaInsets.bottom, width: view.frame.size.width-60, height: 55)
+    
           }
 
     func getRandomPhoto() {
@@ -52,3 +49,25 @@ class ViewController: UIViewController {
     
 }
 
+//        private let button1 = UIButton(frame: CGRect(x: 100,
+//                                                  y: 800,
+//                                                  width: 200,
+//                                                  height: 60))
+//              button1.setTitle("Save",
+//                              for: .normal)
+//              button1.setTitleColor(.systemBlue,
+//                                   for: .normal)
+//
+//              button1.addTarget(self,
+//                               action: #selector(buttonAction),
+//                               for: .touchUpInside)
+//
+//              self.view.addSubview(button)
+//
+//              // Do any additional setup after loading the view.
+//          }
+//}
+//
+//          @objc
+//          func buttonAction() {
+//              print("Button pressed")
