8071a6f4da45340928711e46f4044363321e7253
jhconger <jhconger@gmail.com>
2022-09-09 16:38:31 -0500

    added save button and placed it below random photo button

--- RandomPhoto/ViewController.swift
+++ RandomPhoto/ViewController.swift
@@ -17,7 +17,13 @@ class ViewController: UIViewController {
         button.setTitleColor(.black, for: .normal)
         return button
     }()
-
+    private let button1: UIButton = {
+        let button1 = UIButton()
+        button1.setTitle("Save",for: .normal)
+        button1.setTitleColor(.systemBlue,for: .normal)
+       
+        return button1
+    }()
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemMint
@@ -25,18 +31,24 @@ class ViewController: UIViewController {
         imageView.frame = CGRect(x: 0, y: 0, width: 300, height: 300)
         imageView.center = view.center
         view.addSubview(button)
+        view.addSubview(button1)
         getRandomPhoto()
         button.addTarget(self, action: #selector(didTapButton), for: .touchUpInside)
+        button1.addTarget(self,action: #selector(buttonAction),for: .touchUpInside)
     }
     @objc func didTapButton() {
         getRandomPhoto()
 
     }
-        override func viewDidLayoutSubviews() {
-            super.viewDidLayoutSubviews()
-            button.frame = CGRect(x: 30, y: view.frame.size.height-150-view.safeAreaInsets.bottom, width: view.frame.size.width-60, height: 55)
-    
-          }
+    @objc func buttonAction() {
+                print("Button pressed")
+    }
+
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        button.frame = CGRect(x: 30, y: view.frame.size.height-150-view.safeAreaInsets.bottom, width: view.frame.size.width-60, height: 55)
+        button1.frame = CGRect(x: 30, y: view.frame.size.height-100-view.safeAreaInsets.bottom, width: view.frame.size.width-60, height: 55)
+    }
 
     func getRandomPhoto() {
         let urlString = "https://source.unsplash.com/random/600X600"
@@ -68,6 +80,4 @@ class ViewController: UIViewController {
 //          }
 //}
 //
-//          @objc
-//          func buttonAction() {
-//              print("Button pressed")
+//
